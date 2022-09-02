a8646e27cbf35ea1b6991379dc5ef537439a1873
Alireza <13579gharavi@gmail.com>
2022-09-02 13:12:06 +0430

    execute app.run() if run.py executed as python file (not imported as a module)

--- run.py
+++ run.py
@@ -7,4 +7,6 @@ app = Flask("__name__")
 app.register_blueprint(api, url_prefix='/AuthorNews/v1')
 
 CORS(app, resources={r"/*": {"origins":"*"}})
-app.run('0.0.0.0')
+
+if __name__ == "__main__" :
+    app.run(host='0.0.0.0', port=5000)
