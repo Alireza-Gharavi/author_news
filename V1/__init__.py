9c6041f70292673ad59205382d200ed55ebccf34
Alireza <13579gharavi@gmail.com>
2022-09-03 17:25:46 +0430

    ssl compatibility

--- V1/__init__.py
+++ V1/__init__.py
@@ -1,14 +1,24 @@
 from flask_restx import Api
-from flask import Blueprint
+from flask import Blueprint, url_for
 
 
 blueprint = Blueprint('authornews',__name__)
 
 
+class MyApi(Api):
+    @property
+    def specs_url(self):
+        """Monkey patch for HTTPS"""
+        scheme = 'http' if '5000' in self.base_url else 'https'
+        return url_for(self.endpoint('specs'), _external=True, _scheme=scheme)
+
+
+
+
 from V1.authornews import api as authnews 
 
 
-api = Api(blueprint,
+api = MyApi(blueprint,
             title='AuthorNews',
             version='1.0',
             description='Some Statiscal Analysis on Crypto News',
