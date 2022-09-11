2f8b438b94459b7d146801e60f0a2a5b8b89f9d9
Alireza <13579gharavi@gmail.com>
2022-09-11 12:31:55 +0430

    added dashboard page for news_count api

--- run.py
+++ run.py
@@ -2,9 +2,16 @@ from flask import Flask
 from V1 import blueprint as api
 from flask_cors import CORS
 
+from V1.plotlydash.dashboard import init_dashboard
+
+
 app = Flask("__name__")
 
+app = init_dashboard(app)
+
 app.register_blueprint(api, url_prefix='/AuthorNews/v1')
+
+
 
 CORS(app, resources={r"/*": {"origins":"*"}})
 
